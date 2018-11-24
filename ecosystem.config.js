0a43f641156a4647af8d8cd1430d8108ffb1cb35
Joe Li <joe.lea@foxmail.com>
2018-11-24 19:07:03 +0800

    add configuration for pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'cors-tester-web',
+      script    : 'web/bin/www',
+      instances: 1,
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4000
+      }
+    },
+    {
+      name      : 'cors-tester-api',
+      script    : 'api/bin/www',
+      instances: 1,
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4001
+      }
+    },{
+      name      : 'cors-tester-api-cors',
+      script    : 'api-cors/bin/www',
+      instances: 1,
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4002
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   *
+   * Step1 (initiate, one time step): pm2 deploy ecosystem.config.js  production setup
+   * Step2:  pm2 deploy ecosystem.config.js  production
+   */
+  deploy : {
+    production : {
+      user : 'joe',
+      host : '182.254.242.31',
+      ref  : 'origin/master',
+      repo : 'git@github.com:choelea/cors-tester.git',
+      path : '/home/joe/nodejsapp/cors-tester',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
