3d3fe831fcb470b3b4a41bd44491abc9abd39cee
Joe Li <joe.lea@foxmail.com>
2018-11-25 20:24:48 +0800

    cors enable test

--- api-cors/app.js
+++ api-cors/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const cors = require('cors');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.use(cors());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- api-cors/routes/users.js
+++ api-cors/routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log('Request For Users Is Excuted..................')
+  res.json([{'name':'Joe'},{'name':'Mark'}]);
 });
 
 module.exports = router;
