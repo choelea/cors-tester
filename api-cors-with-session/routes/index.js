a2ca7f5fb7fccf28cfdabb42c9bd1f513f80b63b
Joe Li <joe.li@okchem.com>
2018-11-26 15:27:34 +0800

    create session

--- api-cors-with-session/routes/index.js
+++ api-cors-with-session/routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  let sess = req.session;
+  console.log(sess.id);
   res.render('index');
 });
 module.exports = router;
