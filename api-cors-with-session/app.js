0fd319ae3bb469f3bfe1304911007fe9d8463eef
Joe Li <joe.li@okchem.com>
2018-11-26 15:21:34 +0800

    try session

--- api-cors-with-session/app.js
+++ api-cors-with-session/app.js
@@ -2,13 +2,18 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var hbs = require('hbs');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const cors = require('cors');
 var viewHistory = require('./routes/viewhistory');
+var index = require('./routes/index');
 var session = require('express-session')
 
 var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(cors());
 app.use(session({
@@ -25,8 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/viewhistories', viewHistory);
-
-
+app.use('/', index);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
